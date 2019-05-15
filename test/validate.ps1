dfb4385f66079213f22b4d3167eafb445542ddde
Bernard <bernard@ducourier.com>
2019-05-15 14:23:36 -0400

    Update validation

--- test/validate.ps1
+++ test/validate.ps1
@@ -1,5 +1,5 @@
 Param(
-    [Parameter(Mandatory = $false)][string]$templateLibraryName = "asg",
+    [Parameter(Mandatory = $false)][string]$templateLibraryName = "resourcegroups",
     [string]$templateName = "azuredeploy.json",
     [string]$Location = "canadacentral",
     [string]$subscription = "2de839a0-37f9-4163-a32a-e1bdb8d6eb7e"
@@ -10,16 +10,38 @@ Param(
 # Execution begins here
 #******************************************************************************
 
+function getValidationURL {
+    $remoteURL = git config --get remote.origin.url
+    $currentBranch = git rev-parse --abbrev-ref HEAD
+    $remoteURLnogit = $remoteURL -replace '\.git', ''
+    $remoteURLRAW = $remoteURLnogit -replace 'github.com', 'raw.githubusercontent.com'
+    $validateURL = $remoteURLRAW + '/' + $currentBranch + '/template/azuredeploy.json'
+    return $validateURL
+}
+
+$currentBranch = git rev-parse --abbrev-ref HEAD
+
+if ($currentBranch -eq 'master') {
+    $confirmation = Read-Host "You are working off the master branch... are you sure you want to validate the template from here? Switch to the dev branch is recommended. Continue? (y/n)"
+    if ($confirmation -ne 'y') {
+        exit
+    }
+}
+
+# Make sure we update code to git
+# git branch dev ; git checkout dev ; git pull origin dev
+git add . ; git commit -m "Update validation" ; git push origin $currentBranch
+
 Select-AzureRmSubscription -Subscription $subscription
 
-# Start the deployment
-Write-Host "Starting validation deployment...";
+# Validating server template
+Write-Host "Starting $templateLibraryName validation deployment...";
 
-New-AzureRmDeployment -Location $Location -Name "Validate-RG" -TemplateUri "https://raw.githubusercontent.com/canada-ca/accelerators_accelerateurs-azure/master/Templates/arm/masterdeploy/20190319.1/masterdeploysub.json" -TemplateParameterFile (Resolve-Path -Path "$PSScriptRoot\parameters\masterdeploysub.parameters.json") -Verbose;
+$validationURL = getValidationURL
+New-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Name "validate-$templateLibraryName-template" -TemplateUri $validationURL -TemplateParameterFile (Resolve-Path "$PSScriptRoot\parameters\validate.parameters.json") -Verbose
 
-$provisionningState = (Get-AzureRmDeployment -Name "Validate-RG").ProvisioningState
+$provisionningState = (Get-AzureRmResourceGroupDeployment -ResourceGroupName PwS2-validate-$templateLibraryName-RG -Name "validate-$templateLibraryName-template").ProvisioningState
 
 if ($provisionningState -eq "Failed") {
-    Write-Host "One of the jobs was not successfully created... exiting..."
-    exit
+    Write-Host  "Test deployment failed..."
 }
