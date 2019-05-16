b2e84e27944bcee462e24f77d50c5ba3a3d31efe
Bernard <bernard@ducourier.com>
2019-05-16 13:09:19 -0400

    Update validation

--- test/validate.ps1
+++ test/validate.ps1
@@ -1,8 +1,9 @@
 Param(
-    [Parameter(Mandatory = $false)][string]$templateLibraryName = "resourcegroups",
-    [string]$templateName = "azuredeploy.json",
+    [Parameter(Mandatory = $false)][string]$templateLibraryName = (Split-Path (Resolve-Path "$PSScriptRoot\..") -Leaf),
     [string]$Location = "canadacentral",
-    [string]$subscription = "2de839a0-37f9-4163-a32a-e1bdb8d6eb7e"
+    [string]$subscription = "",
+    [switch]$devopsCICD = $false,
+    [switch]$doNotCleanup = $false
 )
 
 #******************************************************************************
@@ -19,29 +20,69 @@ function getValidationURL {
     return $validateURL
 }
 
-$currentBranch = git rev-parse --abbrev-ref HEAD
-
-if ($currentBranch -eq 'master') {
-    $confirmation = Read-Host "You are working off the master branch... are you sure you want to validate the template from here? Switch to the dev branch is recommended. Continue? (y/n)"
-    if ($confirmation -ne 'y') {
-        exit
-    }
+function getBaseParametersURL {
+    $remoteURL = git config --get remote.origin.url
+    $currentBranch = git rev-parse --abbrev-ref HEAD
+    $remoteURLnogit = $remoteURL -replace '\.git', ''
+    $remoteURLRAW = $remoteURLnogit -replace 'github.com', 'raw.githubusercontent.com'
+    $baseParametersURL = $remoteURLRAW + '/' + $currentBranch + '/test/parameters/'
+    return $baseParametersURL
 }
 
-# Make sure we update code to git
-# git branch dev ; git checkout dev ; git pull origin dev
-git add . ; git commit -m "Update validation" ; git push origin $currentBranch
+$currentBranch = "dev"
+$validationURL = "https://raw.githubusercontent.com/canada-ca-azure-templates/$templateLibraryName/dev/template/azuredeploy.json"
+$baseParametersURL = "https://raw.githubusercontent.com/canada-ca-azure-templates/$templateLibraryName/dev/test/"
 
-Select-AzureRmSubscription -Subscription $subscription
+if (-not $devopsCICD) {
+    $currentBranch = git rev-parse --abbrev-ref HEAD
+
+    if ($currentBranch -eq 'master') {
+        $confirmation = Read-Host "You are working off the master branch... are you sure you want to validate the template from here? Switch to the dev branch is recommended. Continue? (y/n)"
+        if ($confirmation -ne 'y') {
+            exit
+        }
+    }
+
+    $validationURL = getValidationURL
+    $baseParametersURL = getBaseParametersURL
+
+    # Make sure we update code to git
+    # git branch dev ; git checkout dev ; git pull origin dev
+    git add ..\. ; git commit -m "Update validation" ; git push origin $currentBranch
+}
+
+if ($subscription -ne "") {
+    Select-AzureRmSubscription -Subscription $subscription
+}
+
+# Cleanup validation resource content in case it did not properly completed and left over components are still lingeringcd
+if (-not $doNotCleanup) {
+    #check for existing resource group
+    $resourceGroup = Get-AzureRmResourceGroup -Name PwS2-validate-$templateLibraryName-RG -ErrorAction SilentlyContinue
+
+    if ($resourceGroup) {
+        Write-Host "Cleanup old $templateLibraryName template validation resources if needed..."
+
+        Remove-AzureRmResourceGroup -Name PwS2-validate-resourcegroups-1-RG -Verbose -Force
+        Remove-AzureRmResourceGroup -Name PwS2-validate-resourcegroups-2-RG -Verbose -Force
+    }
+}
 
 # Validating server template
 Write-Host "Starting $templateLibraryName validation deployment...";
 
-$validationURL = getValidationURL
-New-AzureRmDeployment -Location canadacentral -Name "validate-$templateLibraryName-template" -TemplateUri $validationURL -TemplateParameterFile (Resolve-Path "$PSScriptRoot\parameters\validate.parameters.json") -Verbose
+New-AzureRmDeployment -Location -Name "validate-$templateLibraryName-template" -TemplateUri $validationURL -TemplateParameterFile (Resolve-Path "$PSScriptRoot\parameters\validate.parameters.json") -Verbose
 
 $provisionningState = (Get-AzureRmDeployment -Name "validate-$templateLibraryName-template").ProvisioningState
 
 if ($provisionningState -eq "Failed") {
     Write-Host  "Test deployment failed..."
+}
+
+# Cleanup validation resource content
+if (-not $doNotCleanup) {
+    Write-Host "Cleanup $templateLibraryName template validation resources...";
+
+    Remove-AzureRmResourceGroup -Name PwS2-validate-resourcegroups-1-RG -Verbose -Force
+    Remove-AzureRmResourceGroup -Name PwS2-validate-resourcegroups-2-RG -Verbose -Force
 }
